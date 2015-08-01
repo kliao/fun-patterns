f73dfcb1aae1b6c5843d4532e3864f0c8af5f7ff
kliao <kevintliao@gmail.com>
2015-08-01 17:17:44 -0400

    class visitor

--- PatternsForFun/Program.cs
+++ PatternsForFun/Program.cs
@@ -10,6 +10,44 @@ namespace PatternsForFun
     {
         static void Main(string[] args)
         {
+            Cat c = new Cat();
+            Dog d = new Dog();
+
+            var visitor = new MakeSoundVisitor();
+            c.Accept(visitor);
+            d.Accept(visitor);
+
+            Console.ReadKey();
         }
+    }
+
+    public abstract class Animal
+    {
+        public abstract void Accept(IAnimalVisitor visitor);
+    }
+
+    public class Cat : Animal {
+        public override void Accept(IAnimalVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+    public class Dog : Animal {
+        public override void Accept(IAnimalVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+
+    public interface IAnimalVisitor
+    {
+        void Visit(Cat cat);
+        void Visit(Dog dog);
+    }
+
+    public class MakeSoundVisitor : IAnimalVisitor
+    {
+        public void Visit(Cat cat) { Console.WriteLine("Meow"); }
+        public void Visit(Dog dog) { Console.WriteLine("Woof"); }
     }
 }
